74bea1d22b543818d3ef5527edf3498b47d3636d
dg-j7i <40632502+dg-j7i@users.noreply.github.com>
2021-06-17 11:24:59 +0200

    fix emailTemplate

--- src/components/order/emailTemplate.ts
+++ src/components/order/emailTemplate.ts
@@ -1,56 +1,53 @@
-export const getEmailTemplate = (order): string => {
+import { IOrderItem } from './orderContext'
 
-let banhmis = []
-let buns = []
+export const getEmailTemplate = (order: IOrderItem[]): string => {
+  const banhmis: string[] = []
+  const buns: string[] = []
 
-// use findAll()
+  // use findAll()
 
-
-order.map(item => {
+  order.map((item) => {
     if (!item) {
-        return
+      return
     }
     switch (item.food) {
-        case 'Banhmi': {
-            const description = `1x Banhmi ${item.main}${getOptions(item.options)}`
-            banhmis.push(description)
-            break;
-        }
-        case 'Bun': {
-            const description = `1x Bun ${item.main}${getOptions(item.options)}`
-            buns.push(description)
-            break;
-        }
-        default:
-            break;
-        }
-    })
+      case 'Banhmi': {
+        const description = `1x Banhmi ${item.main}${getOptions(item.options)}`
+        banhmis.push(description)
+        break
+      }
+      case 'Bun': {
+        const description = `1x Bun ${item.main}${getOptions(item.options)}`
+        buns.push(description)
+        break
+      }
+      default:
+        break
+    }
+  })
 
-    const template = `
+  const template = `
 Hello hello🙂
 Ich will gerne eine Bestellung aufgeben, um diese heute um 12:00 abzuholen:
 
-${banhmis.map(banhmi => banhmi)}
-${buns.map(bun => bun)}
+${banhmis.map((banhmi) => banhmi)}
+${buns.map((bun) => bun)}
 
 Bitte gebt mir kurz eine Bestätigung, ob die Bestellung für heute 12:00 Uhr möglich ist. 
 Freue mich, danke und bis später
 Joel
 `
-    return template
+  return template
 }
 
-const getOptions = (options) => {
-    if (options.length) {
-        return ""
-    }
+const getOptions = (options: string[]) => {
+  if (!options.length) {
+    return ''
+  }
 
-    const filteredOptions = Object.keys(options).filter(option => !options[option])
-    const opts = filteredOptions.map((option) => `ohne ${option}`)
+  if (options.length === 1) {
+    return ` (${options[0]})`
+  }
 
-    if (!opts.length) {
-        return ""
-    }
-
-    return ` (${opts.join(', ')})`
+  return ` (${options.join(', ')})`
 }
